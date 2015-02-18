aae7eb0669a5e4da60fac4bb4554c12bb7189e2c
bmikelaitis <branbun2@gmail.com>
2015-02-17 17:35:08 -0700

    Created by Coding Ground Client

---
+++ New Project/DogTestDrive.java
@@ -0,0 +1,16 @@
+public class DogTestDrive{
+
+     public static void main(String []args){
+         
+         Dog one = new Dog();
+         one.size = 90;
+         Dog two = new Dog();
+         two.size = 8;
+         Dog three = new Dog();
+         three.size = 35;
+         
+         one.bark();
+         two.bark();
+         three.bark();
+     }  
+}     
